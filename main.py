bb5985083431b3a81204254902f831320a96b9da
Fenway Powers <31604245+fenwaypowers@users.noreply.github.com>
2023-06-04 01:56:24 -0400

    Working

---
+++ main.py
@@ -0,0 +1,57 @@
+import subprocess, argparse, os
+
+def main():
+    global output_path
+    global base_path
+    global book_title
+
+    parser = argparse.ArgumentParser(description='Takes MP3 files downloaded from Libro.fm and concatenates them into one MP3 file with generated chapters.')
+    parser.add_argument('-d','--dir', help='Specify the directory where the MP3 files you wish to concatenate. The directory name should be the same as the title of the audiobook as in the MP3 files.', default='.')
+
+    args = parser.parse_args()
+    os.chdir(args.dir)
+
+    cwd = os.getcwd()
+    book_title = os.path.basename(cwd)
+
+    output_path = os.path.join(cwd, f"{book_title}.mka")
+    base_path = os.path.join(cwd, f"{book_title} - Track")
+
+    cmd = "mkvmerge --ui-language en_US --priority lower --output '{}' --language 0:und".format(output_path)
+
+    cmd = add_tracks(cmd)
+    append_sections = append()
+    cmd = finishcmd(cmd, append_sections)
+    
+    # Run the command
+    subprocess.run(cmd, shell=True)
+
+def add_tracks(cmd: str):
+    # Add all tracks to cmd
+    for i in range(1, 25):
+        track_number = f"{i:03}"
+        track_path = f"{base_path} {track_number}.mp3"
+        cmd += " '(' '{}' ')' +".format(track_path)
+
+    # Remove trailing '+' from the command
+    cmd = cmd[:-2]
+    return cmd
+
+def append():
+    append_sections = ""
+    # Append --append-to sections
+    for i in range(1, 24):
+        append_section = f"{i}:0:{i-1}:0"
+        append_sections += f",{append_section}"
+
+    # Remove first comma
+    append_sections = append_sections[1:]
+    return append_sections
+
+def finishcmd(cmd: str, append_sections: str):
+    # Finish up the command
+    cmd += " --chapter-language und --generate-chapters-name-template 'Chapter <NUM:2>' --generate-chapters when-appending --append-to {}".format(append_sections)
+    return cmd
+
+if __name__ == "__main__":
+    main()
